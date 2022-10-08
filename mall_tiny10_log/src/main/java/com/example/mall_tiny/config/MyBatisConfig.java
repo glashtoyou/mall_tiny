80839d87bfc8344934027cc846088ef061e2fe38
glashtoyou <2968969369@qq.com>
2022-10-08 21:33:37 +0800

    log

---
+++ mall_tiny10_log/src/main/java/com/example/mall_tiny/config/MyBatisConfig.java
@@ -0,0 +1,13 @@
+package com.example.mall_tiny.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis配置类
+ * Created by macro on 2019/4/8.
+ */
+@Configuration
+@MapperScan({"com.example.mall_tiny.mbg.mapper","com.example.mall_tiny.dao"})
+public class MyBatisConfig {
+}
